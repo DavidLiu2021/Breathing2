fd44ee24bf85f74f989a0e06f7985f5fa6e5bd85
David_Liu <lzy625842840@163.com>
2024-05-19 16:54:34 -0700

    2 animation transition

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -6,11 +6,10 @@ public class AnimationController : MonoBehaviour
 {
     public AudioSource audioSource;
     public Animator animator;
-    public string animationName;
     public float loudnessThreshold = 0.1f;
     public AudioLoudnessDetection detector;
 
-    private bool animationTriggered = false;
+    private bool isPlayingAnimation = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +21,36 @@ public class AnimationController : MonoBehaviour
     void Update()
     {
         float loudness = detector.GetLoudnessFromAudioClip(audioSource.timeSamples, audioSource.clip);
-        Debug.Log("Loudness: " + loudness);
-        Debug.Log("Loudness Threshold: " + loudnessThreshold);
+        //Debug.Log("Loudness: " + loudness);
+        //Debug.Log("Loudness Threshold: " + loudnessThreshold);
 
-        if (loudness > loudnessThreshold && !animationTriggered){
-            animator.SetTrigger("PlayTrigger");
-            animationTriggered = true;
-            Debug.Log("Playing Animation: " + animationName);
+        // Check if Blooming animation is playing/finished
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Blooming") &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f){
+                isPlayingAnimation = false;
+            }
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Closing") &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f){
+                isPlayingAnimation = false;
+            }
+        
+        
+         if (loudness > loudnessThreshold && !isPlayingAnimation)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || animator.GetCurrentAnimatorStateInfo(0).IsName("Closing"))
+            {
+                Debug.Log("Playing Blooming animation");
+                animator.SetTrigger("PlayBlooming");
+                isPlayingAnimation = true;
+            }
+            else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Blooming"))
+            {
+                Debug.Log("Playing Closing animation");
+                animator.SetTrigger("PlayClosing");
+                isPlayingAnimation = true;
+            }
         }
 
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f &&
-            animator.GetCurrentAnimatorStateInfo(0).IsName("Blooming")){
-            animationTriggered = false;
-        }
     }
 }
