68bc228939f8319d3eee03d23043cfe941376f20
David_Liu <lzy625842840@163.com>
2024-05-23 11:35:18 -0700

    PitchAnimationcontroller

---
+++ Assets/Scripts/LoudnessAnimationController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// get loudness info from AudioLoudnessDetection, and control the animator
+public class AnimationController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Animator animator;
+    public float loudnessThreshold = 0.1f;
+    public AudioLoudnessDetection detector;
+
+    private bool isPlayingAnimation = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float loudness = detector.GetLoudnessFromAudioClip(audioSource.timeSamples, audioSource.clip);
+        //Debug.Log("Loudness: " + loudness);
+        //Debug.Log("Loudness Threshold: " + loudnessThreshold);
+
+        // Check if Blooming animation is playing/finished
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Blooming") &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f){
+                isPlayingAnimation = false;
+            }
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Closing") &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f){
+                isPlayingAnimation = false;
+            }
+        
+        
+         if (loudness > loudnessThreshold && !isPlayingAnimation)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || animator.GetCurrentAnimatorStateInfo(0).IsName("Closing"))
+            {
+                Debug.Log("Playing Blooming animation");
+                animator.SetTrigger("PlayBlooming");
+                isPlayingAnimation = true;
+            }
+            else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Blooming"))
+            {
+                Debug.Log("Playing Closing animation");
+                animator.SetTrigger("PlayClosing");
+                isPlayingAnimation = true;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PitchAnimationController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchAnimationController : MonoBehaviour
+{
+    //Get frequency information from AudioPeer to use pitch/frequency to control the Fox animation
+    
+    //public Renderer objectRenderer;
+    public Animator animator;
+    public float transitionDuration = 0.1f;
+
+    private int walkHash;
+    private int runHash;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkHash = Animator.StringToHash("Walk");
+        runHash = Animator.StringToHash("Run");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lowFreqBandValue = GetAverageFreqValue(0, 3);
+        float highFreqBandValue = GetAverageFreqValue(4, 7);
+
+        if (highFreqBandValue > 0.1f){
+            animator.CrossFade(runHash, transitionDuration);
+        }else if (lowFreqBandValue > 0.1f){
+            animator.CrossFade(walkHash, transitionDuration);
+        }
+    }
+
+    float GetAverageFreqValue(int startBand, int endBand){
+        float sum = 0;
+        for (int i = startBand; i <= endBand; i++){
+            sum += AudioPeer._freqBand[i];
+        }
+        return sum / (endBand - startBand + 1);
+    }
+}
