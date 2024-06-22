b9f8ac0116715ab2447ad4d93f67190a8a8aac34
David_Liu <lzy625842840@163.com>
2024-06-22 13:47:20 -0700

    try to change some jump settings

--- Assets/Scripts/FoxPitchAnimationController.cs
+++ Assets/Scripts/FoxPitchAnimationController.cs
@@ -8,8 +8,9 @@ public class FoxPitchAnimationController : MonoBehaviour
     
     // animation control
     public Animator animator;
-    public float transitionDuration = 0.2f;
-    public float toJumpDuration = 0.1f;
+    // public float transitionDuration = 0.2f;
+    // public float toJumpDuration = 0.1f;
+    public float jumpDuration = 1.5f;
 
     // transform control
     public float runspeed = 6f;
@@ -43,23 +44,24 @@ public class FoxPitchAnimationController : MonoBehaviour
         float highFreqBandValue = GetAverageFreqValue(4, 7);
 
         if (highFreqBandValue > 0.1f && (!InGapTrigger || isGrounded)){
-            animator.CrossFade(runHash, transitionDuration);
+            animator.CrossFade(runHash, 0.01f);
             transform.Translate(direction * runspeed * Time.deltaTime);
         }else if (lowFreqBandValue > 0.1f && (!InGapTrigger || isGrounded)){
-            animator.CrossFade(walkHash, transitionDuration);
+            animator.CrossFade(walkHash, 0.01f);
             transform.Translate(direction * walkspeed * Time.deltaTime);
         }
 
         // detect space key pressed
         if (Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space key pressed");
             Jump();
         }
 
         // jump across the gap
         if (InGapTrigger && Input.GetKeyDown(KeyCode.Space)){
-            JumpGap();
+            // JumpGap();
+            StartCoroutine(JumppGap());
         }
-
     }
 
     float GetAverageFreqValue(int startBand, int endBand){
@@ -85,7 +87,7 @@ public class FoxPitchAnimationController : MonoBehaviour
     public void Jump(){
         if (isGrounded){
             rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-            animator.CrossFade(jumpHash, toJumpDuration);
+            animator.CrossFade(jumpHash, 0.0001f);
             isGrounded = false;
         }
     }
@@ -104,6 +106,24 @@ public class FoxPitchAnimationController : MonoBehaviour
 
     void JumpGap(){
         transform.position = GapLand02.position + new Vector3(0, 1, 0);
-        animator.CrossFade(jumpHash, toJumpDuration);
+        animator.CrossFade(jumpHash, 0.1f);
+    }
+
+    IEnumerator JumppGap()
+    {
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = GapLand02.position + new Vector3(0, 1, 0);
+        float elapsedTime = 0f;
+
+        animator.CrossFade(jumpHash, 0.01f); // 假设过渡时间为0.1秒
+
+        while (elapsedTime < jumpDuration)
+        {
+            transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / jumpDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPosition; // 确保到达最终位置
     }
 }
