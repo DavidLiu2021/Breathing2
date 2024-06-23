ae771a74fa32e32250316bc7a8389e4e2ce25f5e
David_Liu <lzy625842840@163.com>
2024-06-23 14:14:02 -0700

    animator change and player controller update

--- Assets/Scripts/FoxPitchAnimationController.cs
+++ Assets/Scripts/FoxPitchAnimationController.cs
@@ -8,15 +8,12 @@ public class FoxPitchAnimationController : MonoBehaviour
     
     // animation control
     public Animator animator;
-    // public float transitionDuration = 0.2f;
-    // public float toJumpDuration = 0.1f;
     public float jumpDuration = 1.5f;
 
     // transform control
     public float runspeed = 6f;
     public float walkspeed  = 5f;
     public float jumpforce = 5f;
-    public Vector3 direction = Vector3.forward;
     public Transform GapLand02;
 
     private int walkHash;
@@ -25,43 +22,44 @@ public class FoxPitchAnimationController : MonoBehaviour
 
     private Rigidbody rb;
     private bool isGrounded = true;
-    private bool InGapTrigger = false;
+    private bool inGapTrigger = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        walkHash = Animator.StringToHash("Walk");
-        runHash = Animator.StringToHash("Run");
-        jumpHash = Animator.StringToHash("Jump");
-
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {     
-        float lowFreqBandValue = GetAverageFreqValue(0, 3);
-        float highFreqBandValue = GetAverageFreqValue(4, 7);
+        // float lowFreqBandValue = GetAverageFreqValue(0, 3);
+        // float highFreqBandValue = GetAverageFreqValue(4, 7);
 
-        if (highFreqBandValue > 0.1f && (!InGapTrigger || isGrounded)){
-            animator.CrossFade(runHash, 0.01f);
-            transform.Translate(direction * runspeed * Time.deltaTime);
-        }else if (lowFreqBandValue > 0.1f && (!InGapTrigger || isGrounded)){
-            animator.CrossFade(walkHash, 0.01f);
-            transform.Translate(direction * walkspeed * Time.deltaTime);
-        }
+        // if (highFreqBandValue > 0.1f && (!inGapTrigger || isGrounded)){
+        //     animator.CrossFade(runHash, 0.01f);
+        //     transform.Translate(Vector3.forward * runspeed * Time.deltaTime);
+        // }else if (lowFreqBandValue > 0.1f && (!inGapTrigger || isGrounded)){
+        //     animator.CrossFade(walkHash, 0.01f);
+        //     transform.Translate(Vector3.forward * walkspeed * Time.deltaTime);
+        // }
+
+        transform.Translate(Vector3.forward * speed() * Time.deltaTime);
 
         // detect space key pressed
-        if (Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log("Space key pressed");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            animator.SetBool("jump", true);
             Jump();
         }
+        animator.SetBool("jump", !isGrounded);
 
         // jump across the gap
-        if (InGapTrigger && Input.GetKeyDown(KeyCode.Space)){
+        if (inGapTrigger && Input.GetKeyDown(KeyCode.Space)){
             // JumpGap();
             StartCoroutine(JumppGap());
         }
+
+        animator.SetFloat("speed", speed());
     }
 
     float GetAverageFreqValue(int startBand, int endBand){
@@ -70,6 +68,19 @@ public class FoxPitchAnimationController : MonoBehaviour
             sum += AudioPeer._freqBand[i];
         }
         return sum / (endBand - startBand + 1);
+    }
+
+    float speed(){
+        float HighPitch = GetAverageFreqValue(3, 7);
+        float LowPitch = GetAverageFreqValue(0, 2);
+
+        if (LowPitch > 0.1f){
+            return walkspeed;
+        }else if (HighPitch > 0.1f){
+            return runspeed;
+        }
+
+        return 0f;
     }
 
     void OnCollisionEnter(Collision collision){
@@ -87,26 +98,26 @@ public class FoxPitchAnimationController : MonoBehaviour
     public void Jump(){
         if (isGrounded){
             rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-            animator.CrossFade(jumpHash, 0.0001f);
+            // animator.SetBool("jump", false);
             isGrounded = false;
         }
     }
 
     void OnTriggerEnter(Collider other){
         if (other.CompareTag("JumpTrigger")){
-            InGapTrigger = true;
+            inGapTrigger = true;
         }
     }
 
     void OnTriggerExit(Collider other){
         if (other.CompareTag("JumpTrigger")){
-            InGapTrigger = false;
+            inGapTrigger = false;
         }
     }
 
     void JumpGap(){
         transform.position = GapLand02.position + new Vector3(0, 1, 0);
-        animator.CrossFade(jumpHash, 0.1f);
+        // animator.CrossFade(jumpHash, 0.1f);
     }
 
     IEnumerator JumppGap()
@@ -115,15 +126,13 @@ public class FoxPitchAnimationController : MonoBehaviour
         Vector3 endPosition = GapLand02.position + new Vector3(0, 1, 0);
         float elapsedTime = 0f;
 
-        animator.CrossFade(jumpHash, 0.01f); // 假设过渡时间为0.1秒
 
-        while (elapsedTime < jumpDuration)
-        {
+        while (elapsedTime < jumpDuration){
             transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / jumpDuration);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        transform.position = endPosition; // 确保到达最终位置
+        transform.position = endPosition; 
     }
 }
