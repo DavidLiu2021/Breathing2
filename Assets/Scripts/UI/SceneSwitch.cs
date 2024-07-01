3d24c40f059a35a2c98f468a5599d2ed1f90ebe8
David_Liu <lzy625842840@163.com>
2024-07-01 12:08:56 -0700

    add 2 more scenes

--- Assets/Scripts/UI/SceneSwitch.cs
+++ Assets/Scripts/UI/SceneSwitch.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
 
+    public void ConfirmLevel(){
+        SceneManager.LoadScene("Selection_Animal");
+    }
+    
     public void ConfirmAnimal(){
         SceneManager.LoadScene("Selection_Landscape");
     }
@@ -13,6 +17,32 @@ public class SceneSwitch : MonoBehaviour
     public void ConfirmLandscape(){
         SceneManager.LoadScene("Selection_Decoration");
     }
+
+    public void ConfirmDecoration(){
+        SceneManager.LoadScene("Map");
+        // StartCoroutine(LoadMapScene());
+    }
+
+    // private IEnumerator LoadMapScene()
+    // {
+    //     AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Map", LoadSceneMode.Additive);
+
+    //     while (!asyncLoad.isDone)
+    //     {
+    //         yield return null;
+    //     }
+
+    //     Scene mapScene = SceneManager.GetSceneByName("Map");
+        
+    //     if (mapScene.IsValid())
+    //     {
+    //         SceneManager.SetActiveScene(mapScene);
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("Failed to load and activate the Map scene.");
+    //     }
+    // }
     
     // Start is called before the first frame update
     void Start()
